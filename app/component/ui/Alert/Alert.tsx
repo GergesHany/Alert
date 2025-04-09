97dcadddc83b403cbcdcfd7c6297b70c1a4caa80
GergesHany <gergeshany179@gmail.com>
2025-04-09 20:12:33 +0200

    Dynamic Alert component & Alert Types Guards

--- app/component/ui/Alert/Alert.tsx
+++ app/component/ui/Alert/Alert.tsx
@@ -1,26 +1,29 @@
 import "./index.scss"
 import { Bell, X } from 'lucide-react';
-
+import type { alertTypes } from "../../../types";
+import type { ReactNode } from "react";
 
 interface IProps {
-  
+  type: alertTypes,
+  title: string,
+  icon: ReactNode,
+  description?: string,
+  children?: ReactNode 
 }
 
-const Alert = ({}: IProps) => {
+const Alert = ({type, title, icon, description, children}: IProps) => {
   return (
-    <div className="alert-danger" >
-      <div className="alert-header">
-        <div className="title">
-          <span> <Bell /> </span> 
-          <h4> Alert Title </h4>  
+    <div className={type}>
+      <div className='alert-header'>
+        <div className='title'>
+          <span> {icon} </span>
+          <h4>{title}</h4>
         </div>
-        <X className="close" size={30} /> 
+        <X size={30} />
       </div>
-      
-      <p> Alert P </p>
-
+        {children ? children : <p> {description} </p>}
     </div>
-  );
-};
+  )
+}
 
 export default Alert;
--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,10 +1,57 @@
 import Alert from "~/component/ui/Alert/Alert";
+import { AlertTriangle, Ban, Bell, CheckCheck, Info } from "lucide-react";
 
 export function Welcome() {
   return (
-    <main >
-       <Alert />
-    </main>
-  );
+    <div className='App'>
+      <Alert 
+        type = "alert-default"
+        title = "Default"
+        icon = {<Bell size={24} />}
+        >
+          <p>
+            This is an alert with default type, 
+            it has a description and a child component and you can add link like this <a href="https://www.google.com">Google</a>
+          </p>
+      </Alert>
+
+     <Alert 
+        type = "alert-info"
+        title = "Default"
+        icon = {<Bell size={24} />}
+        >
+          <p>
+          This is an alert with info type, 
+            it has a description and a child component and you can add link like this <a href="https://www.google.com">Google</a>
+          </p>
+      </Alert>
+
+     <Alert 
+        type = "alert-warning"
+        title = "Default"
+        icon = {<Bell size={24} />}
+        >
+          <p>
+          This is an alert with warning type , 
+            it has a description and a child component and you can add link like this <a href="https://www.google.com">Google</a>
+          </p>
+      </Alert>
+
+
+      <Alert 
+        type = {"alert-error"}
+        title = {"Error"}
+        icon = {<Ban size={24} />}
+        description = "This is an alert with error type"
+      />
+
+      <Alert 
+        type = {"alert-success"}
+        title = {"Success"}
+        icon = {<CheckCheck size={24} />}
+        description = "This is an alert with success type"
+      />
+    </div>
+  )
 }
 
