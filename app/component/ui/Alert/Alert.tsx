01a5763fb420f3ceb8271201cae7b914943edfec
GergesHany <gergeshany179@gmail.com>
2025-04-09 19:12:19 +0200

    Alert Style using sass

--- app/component/ui/Alert/Alert.tsx
+++ app/component/ui/Alert/Alert.tsx
@@ -8,11 +8,17 @@ interface IProps {
 
 const Alert = ({}: IProps) => {
   return (
-    <div className="alert-wrapper" >
-      <Bell />
-      Alert Title
+    <div className="alert-danger" >
+      <div className="alert-header">
+        <div className="title">
+          <span> <Bell /> </span> 
+          <h4> Alert Title </h4>  
+        </div>
+        <X className="close" size={30} /> 
+      </div>
+      
       <p> Alert P </p>
-      <span> <X /> </span>
+
     </div>
   );
 };
--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -3,7 +3,6 @@ import Alert from "~/component/ui/Alert/Alert";
 export function Welcome() {
   return (
     <main >
-       <h2> Hello from react-app </h2>      
        <Alert />
     </main>
   );
